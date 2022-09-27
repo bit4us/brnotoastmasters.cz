14292d4cd1fd19d92326f36759e028cbb6a79001
bit4us <bit4us@gmail.com>
2022-09-27 17:37:03 +0200

    attempt Fix meetingData

--- home/indexNew.php
+++ home/indexNew.php
@@ -13,6 +13,7 @@
 $json = file_get_contents('../data/common.json');
 $json_data = json_decode($json);
 
+$meetingData = $json_data->MeetingData;
 ?>
 
 
@@ -206,7 +207,7 @@ $json_data = json_decode($json);
                 <span class="carousel-title-normal">of your own voice</span>
               </h2>
               <p class="carousel-subtitle-v5 border-top-bottom margin-top-30 margin-bottom-30" data-animation="animated fadeInDown">
-                When: Every <?=$json_data->MeetingData->MeetingDay?>, <?=$json_data->MeetingData->MeetingTime?> <br>Where: <a href="<?=$json_data->MeetingData->MeetingAddressMap?>" target="_blank" rel="noopener noreferrer" style="color: #1B8093; font-weight: bold;"><i class="fa fa-globe"></i> <?=$json_data->MeetingData->MeetingPlaceName?></a></p>
+                When: Every <?=$json_data->meetingData->MeetingDay?>, <?=$json_data->MeetingData->MeetingTime?> <br>Where: <a href="<?=$json_data->MeetingData->MeetingAddressMap?>" target="_blank" rel="noopener noreferrer" style="color: #1B8093; font-weight: bold;"><i class="fa fa-globe"></i> <?=$json_data->MeetingData->MeetingPlaceName?></a></p>
               <a class="carousel-btn-green" data-animation="animated fadeInUp"
                 href="https://bt.tmclub.eu/profile.php?mode=register&YerThere=true" target="_blank">Join Now!</a>
                 <br><br><br>
