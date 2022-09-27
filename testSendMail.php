5c2ef862b0e12e33e86179ba4d3bbb49454c0982
bit4us <bit4us@gmail.com>
2022-09-27 23:39:13 +0200

    Update 2 testSendMail

--- testSendMail.php
+++ testSendMail.php
@@ -4,8 +4,8 @@ $to = 'bit4us@gmail.com';
 $subject = 'Mail sent from sendmail PHP script';
 
 $from = 'treasurer@brnotoastmasters.com';
-$headers = "From: $from";
-$headers .= " MIME-Version: 1.0\r\n";
+$headers = "From: $from \r\n";
+$headers .= "MIME-Version: 1.0\r\n";
 $headers .= "Content-Type: text/html; charset=UTF-8\r\n";
 
 $message = '<p><strong>Sendmail in PHP with HTML content. </strong></p>';
