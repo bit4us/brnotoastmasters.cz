e9a1f632b13c40ca10b453dcb57286b77a98350b
bit4us <bit4us@gmail.com>
2022-09-28 00:04:30 +0200

    test SendMail message update

--- testSendMail.php
+++ testSendMail.php
@@ -10,7 +10,7 @@ $headers .= "MIME-Version: 1.0\r\n";
 $headers .= "Content-Type: text/html; charset=UTF-8\r\n";
 
 $message = "<h3>Reminder to renew your subscription with Brno Toastmasters club</h3>";
-$message .= "<p>Dear ". $name .",</p>";
+$message .= "<p>Dear $name ,</p>";
 $message .= "<p>We value your activity within our club and we are glad to have you as a member of our club.";
 $message .= "<br/>Your input and your progress is really valuable for us and we thank you for being with us so far.";
 $message .= "But now it's about time to renew your subscription with our club in order to keep continuing your access ";
