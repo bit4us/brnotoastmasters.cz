5d545c71a66305282645fb3df0ad7398c5486030
bit4us <bit4us@gmail.com>
2022-09-27 23:32:43 +0200

    testingSendMail.php

---
+++ testSendMail.php
@@ -0,0 +1,18 @@
+<?php
+$to = 'bit4us@gmail.com';
+
+$subject = 'Mail sent from sendmail PHP script';
+
+$from = 'treasurer@brnotoastmasters.com';
+$headers = "From: $from";
+$headers .= "MIME-Version: 1.0\r\n";
+$headers .= "Content-Type: text/html; charset=UTF-8\r\n";
+
+$message = '<p><strong>Sendmail in PHP with HTML content. </strong></p>';
+
+if (mail($to, $subject, $message, $headers)) {
+    echo 'Mail sent successfully.';
+} else {
+    echo 'Unable to send mail. Please try again.';
+}
+?>
